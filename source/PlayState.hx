5c971dc7ebcd59e2b50cb828eee12a80df3c10ca
Jaron <jaronritter01@gmail.com>
2022-02-10 19:59:02 -0600

    add the hero class

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,6 +7,7 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
+		var ourHero:Hero = new Hero(300, 300, "John");
 	}
 
 	override public function update(elapsed:Float)
