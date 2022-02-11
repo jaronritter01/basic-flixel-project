e241879f43fee3b2d381ac2e49d83af308c4ea40
Jaron <jaronritter01@gmail.com>
2022-02-10 19:54:41 -0600

    deleted a file

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,8 +7,6 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
-		var ourHero:Hero = new Hero(300, 275, "John");
-		add(ourHero);
 	}
 
 	override public function update(elapsed:Float)
