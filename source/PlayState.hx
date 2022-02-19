9bb7752c05beff9ff50e5b7403c45d4ad9fd7cdb
Jaron <jaronritter01@gmail.com>
2022-02-19 00:36:24 -0600

    added a super cool text object

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,8 @@
 package;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
@@ -9,6 +11,11 @@ class PlayState extends FlxState
 		super.create();
 		var ourHero:Hero = new Hero(300, 300, "John");
 		add(ourHero);
+
+		var helloWorldText = new FlxText(200, 200, -1);
+		helloWorldText.text = "Hello World!";
+		helloWorldText.setFormat("assets/fonts/AkayaTelivigala-Regular.ttf", 60, FlxColor.BLUE, "center", FlxTextBorderStyle.SHADOW);
+		add(helloWorldText);
 	}
 
 	override public function update(elapsed:Float)
